cb0d9862b8c5e575d9e139fb00278d7a78f26101
Andrea Bartolucci <andreaio.it@hotmail.it>
2015-01-05 15:21:56 +0100

    Aggiunto modulo per caricare mappe da file

---
+++ mapper.py
@@ -0,0 +1,29 @@
+import sys
+import getopt
+import math
+
+def tp(x,y):
+    return math.sqrt(x*x+y*y)
+
+def parseMapFile(map_path):
+    map = []
+    with open(map_path, "r") as map_file:
+        for line in map_file:
+            line = line.split()
+            if len(line) == 0:
+                continue
+            if line[0] == '#':
+                continue
+            if line[0] == 'l':
+                dx =float(line[3])-float(line[1])
+                dy =float(line[4])-float(line[2])
+                d = tp(dx,dy)
+                i = 5.0
+                map.append((float(line[1]),float(line[2])))
+                while i < d:
+                    map.append((float(line[1])+dx*i/d,float(line[2])+dy*i/d))
+                    i=i+10
+                map.append((float(line[3]),float(line[4])))
+            else:
+                map.append((float(line[0]), float(line[1])))
+    return map
