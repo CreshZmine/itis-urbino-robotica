7c1fb3ddeab3b4db53a6d453f2ff86ba5c707083
Andrea Bartolucci <andreaio.it@hotmail.it>
2015-01-16 18:20:12 +0100

    Aggiunta funzione per trovare la strada più breve

--- client_labirinto/nodi.py
+++ client_labirinto/nodi.py
@@ -11,3 +11,41 @@ class nodo:
         dx *= dx
         dy *= dy
         return math.sqrt(dx+dy)
+
+def calcola_strada_corta(inizio, fine):
+    scanning = []
+    scanned = [inizio]
+    for vicino in inizio.vicini:
+        vicino.precendente = inizio
+        vicino.peso = inizio.calcola_distanza(vicino)
+        scanning.append(vicino)
+
+    while scanning:
+        min = scanning[0]
+        for i in range(len(scanning)):
+            if scanning[i].peso < min.peso:
+                min = scanning[i]
+
+        #calcolo i pesi dei vicini di min
+        for vicino in min.vicini:
+            if not vicino in scanned:
+                peso_compl = min.calcola_distanza(vicino)+min.peso
+                if not(hasattr(vicino, "peso")) or peso_compl < vicino.peso:
+                    vicino.peso = peso_compl
+                    vicino.precendente = min
+                if not vicino in scanning:
+                    scanning.append(vicino)
+
+        #rimuovo min da scanning
+        scanning.remove(min)
+        scanned.append(min)
+
+    strada = []
+    if hasattr(fine, "precendente"):
+        strada.append(fine)
+        curr = fine
+        while not curr is inizio:
+            strada.insert(0, curr.precendente)
+            curr = curr.precendente
+
+    return strada
