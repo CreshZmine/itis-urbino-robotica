dc08ed0e2719c343823f9b90df6c607b42fcf6de
Andrea Bartolucci <andreaio.it@hotmail.it>
2015-01-18 17:17:23 +0100

    Aggiunta classe Grafo

--- client_labirinto/nodi.py
+++ client_labirinto/nodi.py
@@ -2,7 +2,7 @@ import math
 
 INFINITE=999
 
-class nodo:
+class Nodo:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -16,48 +16,52 @@ class nodo:
         dy *= dy
         return math.sqrt(dx+dy)
 
-def calcola_strada_corta(inizio, fine):
+class Grafo:
+    def __init__(self):
+        self.nodi = []
 
-    scanning = []
-    scanned = [inizio]
-    for vicino in inizio.vicini:
-        vicino.precedente = inizio
-        vicino.peso = inizio.calcola_distanza(vicino)
-        scanning.append(vicino)
+    def calcola_strada_corta(self, inizio, fine):
+        self.reset_nodi()
+        scanning = []
+        scanned = [inizio]
+        for vicino in inizio.vicini:
+            vicino.precedente = inizio
+            vicino.peso = inizio.calcola_distanza(vicino)
+            scanning.append(vicino)
 
-    while scanning:
-        min = scanning[0]
-        for i in range(len(scanning)):
-            if scanning[i].peso < min.peso:
-                min = scanning[i]
+        while scanning:
+            min = scanning[0]
+            for i in range(len(scanning)):
+                if scanning[i].peso < min.peso:
+                    min = scanning[i]
 
-        #calcolo i pesi dei vicini di min
-        for vicino in min.vicini:
-            if not vicino in scanned:
-                peso_compl = min.calcola_distanza(vicino)+min.peso
-                if peso_compl < vicino.peso:
-                    vicino.peso = peso_compl
-                    vicino.precedente = min
-                if not vicino in scanning:
-                    scanning.append(vicino)
+            #calcolo i pesi dei vicini di min
+            for vicino in min.vicini:
+                if not vicino in scanned:
+                    peso_compl = min.calcola_distanza(vicino)+min.peso
+                    if peso_compl < vicino.peso:
+                        vicino.peso = peso_compl
+                        vicino.precedente = min
+                    if not vicino in scanning:
+                        scanning.append(vicino)
 
-        #rimuovo min da scanning
-        scanning.remove(min)
-        scanned.append(min)
+            #rimuovo min da scanning
+            scanning.remove(min)
+            scanned.append(min)
 
-    strada = []
-    if not fine.precedente is None:
-        strada.append(fine)
-        curr = fine
-        while not curr is inizio:
-            strada.insert(0, curr.precedente)
-            curr = curr.precedente
+        strada = []
+        if not fine.precedente is None:
+            strada.append(fine)
+            curr = fine
+            while not curr is inizio:
+                strada.insert(0, curr.precedente)
+                curr = curr.precedente
 
-    return strada
+        return strada
 
-def reset_nodi(nodi):
-    #Resetto gli i pesi e i puntatori ai nodi precedenti
-    for n in nodi:
-        n.peso = INFINITE
-        n.precedente = None
+    def reset_nodi(self):
+        #Resetto gli i pesi e i puntatori ai nodi precedenti
+        for n in self.nodi:
+            n.peso = INFINITE
+            n.precedente = None
 
