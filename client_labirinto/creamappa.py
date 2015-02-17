90bbe685fa9e9928546a284f693e1c5e7e640654
Andrea Bartolucci <bartol@gmail.com>
2015-02-17 16:06:01 +0100

    Programma per trovare la mappa in cui è il robot

---
+++ client_labirinto/creamappa.py
@@ -0,0 +1,47 @@
+import movimenti
+import math
+import pyglet
+from pyglet.gl import *
+
+robot = movimenti.Robo_moves()
+mappa=[[False for x in range(400)] for x in range(400)]
+x = 200
+y = 200
+theta = math.pi/2
+
+def elabora_sensore():
+    dist = float(robot.sense(0))
+    x_coor = x+dist*math.cos(theta)
+    y_coor = y+dist*math.sin(theta)
+    if x_coor > 400 or x_coor < 0 or y_coor > 400 or y_coor < 0:
+        mappa[int(x_coor)][int(y_coor)] = True
+
+def quadro():
+    d = 4.0
+    for x in range(400):
+        for y in range(400):
+            if mappa[i][j] == True:
+                glBegin(GL_LINE_STRIP)
+                glVertex2f(x-d, y-d)
+                glVertex2f(x-d, y+d)
+                glVertex2f(x+d, y+d)
+                glVertex2f(x+d, y-d)
+                glVertex2f(x-d, y-d)
+                glEnd()
+
+win = pyglet.window.Window()
+
+while theta <  math.pi*5/2:
+    elabora_sensore()
+    robot.turn(0.052)
+    theta += 0.052
+    print str(theta)
+
+robot.termina()
+
+@win.event
+def on_draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+    quadro()
+
+pyglet.app.run()
