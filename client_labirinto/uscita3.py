53113c3f46e369c0ef60a2ee6f955df51a68cdae
Andrea Bartolucci <andreaio.it@hotmail.it>
2015-04-06 01:22:06 +0200

    Modificate le funzioni in modo da ragionare con una mappa fatta da caselle di 30cm e con una nuova lista per i muri

--- client_labirinto/uscita3.py
+++ client_labirinto/uscita3.py
@@ -14,13 +14,14 @@ MAX_MAP e' la grandezza della mappa
 MAX_MAP = 400
 
 '''
+Una "casella" e' di 0.033 (1/30) cm
+'''
+
+'''
 robot[0] = x
 robot[1] = y
 '''
 robot = [MAX_MAP/2, MAX_MAP/2]
-
-robo_dx = 0
-robo_dy = 1
 
 mov = RoboSerial.RoboSerial()
 mov.openConnection()
@@ -28,30 +29,29 @@ mov.openConnection()
 sensori_distanza = []
 sensori_distanza.append(sensore.Sensore(1, mov))    #Sensore davanti
 sensori_distanza[-1].dx = 0
-sensori_distanza[-1].dy = 5
+sensori_distanza[-1].dy = 5/30.0
 sensori_distanza[-1].offset = 0
 sensori_distanza.append(sensore.Sensore(2, mov))    #Sensore destra-alto
-sensori_distanza[-1].dx = 7.5
-sensori_distanza[-1].dy = 5
+sensori_distanza[-1].dx = 7.5/30.0
+sensori_distanza[-1].dy = 5/30.0
 sensori_distanza[-1].offset = -math.pi/2
 sensori_distanza.append(sensore.Sensore(3, mov))    #Sensore destra-basso
-sensori_distanza[-1].dx = 7.5
-sensori_distanza[-1].dy = -5
+sensori_distanza[-1].dx = 7.5/30.0
+sensori_distanza[-1].dy = -5/30.0
 sensori_distanza[-1].offset = -math.pi/2
 sensori_distanza.append(sensore.Sensore(4, mov))    #Sensore sinistra-alto
-sensori_distanza[-1].dx = -7.5
-sensori_distanza[-1].dy = 5
+sensori_distanza[-1].dx = -7.5/30.0
+sensori_distanza[-1].dy = 5/30.0
 sensori_distanza[-1].offset = math.pi/2
 sensori_distanza.append(sensore.Sensore(5, mov))    #Sensore sinistra-basso
-sensori_distanza[-1].dx = -7.5
-sensori_distanza[-1].dy = -5
+sensori_distanza[-1].dx = -7.5/30.0
+sensori_distanza[-1].dy = -5/30.0
 sensori_distanza[-1].offset = math.pi/2
 
 sensore_angolo = sensore.Sensore(6, mov)
 sensore_luminosita = sensore.Sensore(7, mov)
 
 sensore_temperatura = sensore.Sensore(8, mov)
-sensore_temperatura.angolo = 0
 
 sensore_velocita = sensore.Sensore(9, mov)
 sensore_distanza_p = sensore.Sensore(10, mov) #Distanza percorsa
@@ -62,17 +62,28 @@ def elabora_sensore(theta, sensore):
     sensore e' il sensore da analizzare
     '''
     dist = sensore.leggi()
+    dist /= 30.0 #Lo porto nella unita di misura "casella"
     x_sensore = robot[0]+sensore.dx*math.cos(theta)-sensore.dy*math.sin(theta)
     y_sensore = robot[1]+sensore.dx*math.sin(theta)+sensore.dy*math.cos(theta)
     x_coor = x_sensore+dist*math.cos(theta+sensore.offset)
     y_coor = y_sensore+dist*math.sin(theta+sensore.offset)
-    if not(int(x_coor) >= MAX_MAP or int(x_coor) < 0 or int(y_coor) >= MAX_MAP or int(y_coor) < 0):
-        grid[int(x_coor)][int(y_coor)] = 1
+
+    x_coor_approx = int(math.ceil(x_coor)) if (x_coor % 1) > 0.5 else int(math.floor(x_coor))
+    y_coor_approx = int(math.ceil(y_coor)) if (y_coor % 1) > 0.5 else int(math.floor(y_coor))
+    prima_casella = (math.trunc(x_coor), math.trunc(y_coor))
+    if maht.fabs(y_coor_approx - y_coor) < maht.fabs(x_coor_approx - x_coor):
+        seconda_casella = (prima_casella[0], prima_casella[1] + (1 if (y_coor % 1) > 0.5 else -1))
+    else:
+        seconda_casella = (prima_casella[0] + (1 if (x_coor % 1) > 0.5 else -1), prima_casella[1])
+    muri += (prima_casella, seconda_casella)
     #imposto le caselle tra la mia posizione e il rilevamento a 0 (vuoto)
     dx, dy = math.cos(theta+sensore.offset), math.sin(theta+sensore.offset)
     cx, cy = dx, dy
     for i in range(dist):
-        grid[int(x_sensore + cx)][y_sensore + cy] = 0
+        try:
+            grid[int(x_sensore + cx)][y_sensore + cy] = 1
+        except IndexError:
+            pass
         cx, cy = cx+dx, cy+dy
 
 def elabora_velocita(theta, distanza_precedente, distanza_corrente, sensore_velocita):
@@ -80,27 +91,29 @@ def elabora_velocita(theta, distanza_precedente, distanza_corrente, sensore_velo
     Aggiorna la posizione del robot tenendo conto della velocita
     '''
     delta_distanza = distanza_corrente - distanza_precedente
+    delta_distanza /= 30.0
     robot[0] += delta_distanza*math.cos(theta)
     robot[1] += delta_distanza*math.sin(theta)
 
 
-def elabora_posizione(theta):
-    '''
-    Aggiorna la mappa considerando che la posizione del robot e'
-    sicuramente libera
-    '''
-    #TODO
-
 #moves = movimenti.Robo_moves()
 
 '''
-0 - vuoto
-1 - muro
+1 - vuoto
 2 - non esplorato
 
 grid viene inizializzato come non esplorato
+Una cella di grid è di 30 cm
 '''
 grid = [[int(2) for x in xrange(MAX_MAP+1)] for y in xrange(MAX_MAP+1)]
+
+'''
+[((1,1),(1,2)),((3,2),(2,2))]
+    ^     ^
+    |     |
+    -------
+'''
+muri = []
 
 theta = sensore_angolo.leggi()*math.pi/180;
 
@@ -112,4 +125,3 @@ while True:
     distanza_corrente = sensore_distanza_p.leggi()
     elabora_velocita(theta, distanza_precedente, distanza_corrente, sensore_velocita)
     distanza_precedente = distanza_corrente
-    elabora_posizione(theta)
