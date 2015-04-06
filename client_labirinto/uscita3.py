22d5f854943b8ac6e8c1cc518c6f579912a0925a
Andrea Bartolucci <andreaio.it@hotmail.it>
2015-04-06 17:17:16 +0200

    Aggiunta eventualità di un'altro piano

--- client_labirinto/uscita3.py
+++ client_labirinto/uscita3.py
@@ -7,11 +7,20 @@ import RoboSerial
 import sensore
 import sys
 import time
+import timer
 
 '''
 MAX_MAP e' la grandezza della mappa
 '''
 MAX_MAP = 400
+
+'''
+RAMP_SECONDS sono i secondi per cui deve salire
+o scendere il robot per considerare quel tratto come rampa
+RAMP_TOLL e' il grado dopo il quale il tratto viene considerato in pendenza
+'''
+RAMP_SECONDS = 2
+RAMP_TOLL = 2
 
 '''
 Una "casella" e' di 0.033 (1/30) cm
@@ -25,6 +34,7 @@ robot = [MAX_MAP/2, MAX_MAP/2]
 
 mov = RoboSerial.RoboSerial()
 mov.openConnection()
+t = timer.Timer()
 
 sensori_distanza = []
 sensori_distanza.append(sensore.Sensore(1, mov))    #Sensore davanti
@@ -55,6 +65,8 @@ sensore_temperatura = sensore.Sensore(8, mov)
 
 sensore_velocita = sensore.Sensore(9, mov)
 sensore_distanza_p = sensore.Sensore(10, mov) #Distanza percorsa
+
+sensore_giroscopio = sensore.Sensore(11, mov)
 
 def elabora_sensore(theta, sensore):
     '''
@@ -95,6 +107,15 @@ def elabora_velocita(theta, distanza_precedente, distanza_corrente, sensore_velo
     robot[0] += delta_distanza*math.cos(theta)
     robot[1] += delta_distanza*math.sin(theta)
 
+def elabora_giroscopio():
+    incl = sensore_giroscopio.leggi()
+    if math.fabs(incl) > RAMP_TOLL:
+        t.start()
+        if t.read() > RAMP_SECONDS: #Ci siamo mossi su un'altra griglia
+            t.stop()
+            grid = grid1 if grid is grid2 else grid2
+    else:
+        t.stop()
 
 #moves = movimenti.Robo_moves()
 
@@ -102,10 +123,13 @@ def elabora_velocita(theta, distanza_precedente, distanza_corrente, sensore_velo
 1 - vuoto
 2 - non esplorato
 
-grid viene inizializzato come non esplorato
+grid1 e 2 vengono inizializzate come non esplorato
+grid e' la griglia corrente
 Una cella di grid è di 30 cm
 '''
-grid = [[int(2) for x in xrange(MAX_MAP+1)] for y in xrange(MAX_MAP+1)]
+grid1 = [[int(2) for x in xrange(MAX_MAP+1)] for y in xrange(MAX_MAP+1)]
+grid2 = [[int(2) for x in xrange(MAX_MAP+1)] for y in xrange(MAX_MAP+1)]
+grid = grid1
 
 '''
 [((1,1),(1,2)),((3,2),(2,2))]
@@ -123,5 +147,6 @@ while True:
     for s in sensori_distanza:
         elabora_sensore(theta, s)
     distanza_corrente = sensore_distanza_p.leggi()
+    elabora_giroscopio()
     elabora_velocita(theta, distanza_precedente, distanza_corrente, sensore_velocita)
     distanza_precedente = distanza_corrente
