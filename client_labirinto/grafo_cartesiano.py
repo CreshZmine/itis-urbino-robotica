ab3ebc74bbff2622eea3acd0a8e960ab6f27ecd7
Andrea Bartolucci <andreaio.it@hotmail.it>
2015-04-06 18:42:12 +0200

    Aggiunto anche l'altro capo del muro

--- client_labirinto/grafo_cartesiano.py
+++ client_labirinto/grafo_cartesiano.py
@@ -8,13 +8,21 @@ class GrafoCartesiano:
         self.pos_out = pos_out
 
     def risolvi(self):
-        coor_muri = map(__trova_coor, self.muri)
+        coor_muri_t = map(__trova_coor, self.muri)
+        coor_muri = []
+        for el in coor_muri_t:
+            coor_muri.append(el[0])
+            coor_muri.append(el[1])
         #TODO
 
     def __trova_coor(self, muro):
         coor = copy.deepcopy(muro[0])
         if muro[0][0] != muro[1][0]: #E' stata modificata la componente x
             coor[0] = muro[0][0] if muro[0][0] > muro[1][0] else muro[1][0]
+            coor1 = copy.deepcopy(coor)
+            coor1[1] += 1
         elif muro[0][1] != muro[1][1]: #E' stata modificata la componente y
             coor[1] = muro[0][1] if muro[0][1] > muro[1][0] else muro[1][1]
-        return coor
+            coor1 = copy.deepcopy(coor)
+            coor1[0] += 1
+        return coor, coor1
