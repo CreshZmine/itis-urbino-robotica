5d1e4dafa7923a7deab911a41b6096dcfd662175
Andrea Bartolucci <andreaio.it@hotmail.it>
2015-04-06 22:49:47 +0200

    Implementato dijkstra per una mappa con coordinate cartesiane

--- client_labirinto/grafo_cartesiano.py
+++ client_labirinto/grafo_cartesiano.py
@@ -2,27 +2,60 @@ import nodi2
 import copy
 
 class GrafoCartesiano:
-    def __init__(self, muri, pos_in, pos_out):
-        self.grafo = nodi2.Grafo()
-        self.pos_in = pos_in
-        self.pos_out = pos_out
 
-    def risolvi(self):
-        coor_muri_t = map(__trova_coor, self.muri)
-        coor_muri = []
-        for el in coor_muri_t:
-            coor_muri.append(el[0])
-            coor_muri.append(el[1])
-        #TODO
+    INFINITY = 999999
 
-    def __trova_coor(self, muro):
-        coor = copy.deepcopy(muro[0])
-        if muro[0][0] != muro[1][0]: #E' stata modificata la componente x
-            coor[0] = muro[0][0] if muro[0][0] > muro[1][0] else muro[1][0]
-            coor1 = copy.deepcopy(coor)
-            coor1[1] += 1
-        elif muro[0][1] != muro[1][1]: #E' stata modificata la componente y
-            coor[1] = muro[0][1] if muro[0][1] > muro[1][0] else muro[1][1]
-            coor1 = copy.deepcopy(coor)
-            coor1[0] += 1
-        return coor, coor1
+    def __init__(self):
+        self.dist = {}
+        self.prev = {}
+        self.unvisited = []
+
+    def __min(self, x, y):
+        return x if self.dist[x] < self.dist[y] else y
+
+    def __vicini(self, muri, pos):
+        vicini = []
+        for x in range(pos[0]-1, pos[0]+2):
+            for y in range(pos[1]-1, pos[1]+2):
+                if not self.__muro_presente(muri, pos, (x,y)) and pos != (x,y):
+                    vicini.append((x,y))
+        return vicini
+
+    def __muro_presente(self, muri, pos, pos1):
+        if (pos, pos1) in muri or (pos1, pos) in muri:
+            return True
+        return False
+
+    def __length(self, pos, pos1):
+        if pos[0] == pos1[0] or pos[1] == pos1[1]:
+            return 1
+        return 1.41
+
+    def risolvi(self, muri, pos_in, pos_out, graph_dimension_x, graph_dimension_y):
+        for x in range(graph_dimension_x):
+            for y in range(graph_dimension_y):
+                self.dist[(x,y)] = INFINITY
+                self.prev[(x,y)] = (None, None)
+                self.unvisited.append((x,y))
+                self.dist[pos_in] = 0
+
+        while len(unvisited) > 0:
+            u = reduce(lambda x, y: self.__min(x, y), self.unvisited)
+            if u == pos_out
+                break
+
+            self.unvisited.remove(u)
+
+            for v in self.__vicini(muri, u):
+                if v in self.unvisited:
+                    alt = self.dist[u] + self.__length(v, u)
+                    if alt < dist[v]:
+                        dist[v] = alt
+                        prev[v] = u
+
+        s = []
+        u = pos_out
+        while self.prev[u] != (None, None):
+            s.insert(0, u)
+            u = self.prev[u]
+        return s, dist[u]
